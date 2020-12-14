2902f662029887393e49b8aa20e5ce43009ae682
tae-sk <68097756+tae-sk@users.noreply.github.com>
2020-12-14 15:19:05 +0900

    add:오타 고치기 and 구분 할 수 있게 숫자 추가 데이터 추가한다.

--- src/main/java/com/taeseok/apis/service/UserService.java
+++ src/main/java/com/taeseok/apis/service/UserService.java
@@ -1,5 +1,6 @@
 package com.taeseok.apis.service;
 
+import com.taeseok.apis.model.User;
 import com.taeseok.apis.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,5 +12,36 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
+    }
+    public void findAll(){
+        for(User user : this.userRepository.findAll()){
+            System.out.println(user);
+        }
+    }
+
+    public void initializeUsers(){
+        User user1 = User.builder()
+                .email("example1@sample.com")
+                .name("Mr. Sample")
+                .phone("01000001234")
+                .build();
+
+        User user2 = User.builder()
+                .email("example2@sample.com")
+                .name("Mrs. Sample")
+                .phone("01000001234")
+                .build();
+
+        User user3 = User.builder()
+                .email("example3@sample.com")
+                .name("ms. Sample data")
+                .phone("01000001234")
+                .build();
+
+        this.userRepository.save(user1);
+        this.userRepository.save(user2);
+        this.userRepository.save(user3);
+        this.userRepository.flush();
+
     }
 }
