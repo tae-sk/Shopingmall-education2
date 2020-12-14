bbdd6111f979987b53384da155099e4d07735ad4
tae-sk <68097756+tae-sk@users.noreply.github.com>
2020-12-14 14:17:39 +0900

    add:UserRepository를 추가한다.

---
+++ src/main/java/com/taeseok/apis/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.taeseok.apis.repository;
+
+import com.taeseok.apis.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
