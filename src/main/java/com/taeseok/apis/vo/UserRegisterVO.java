75f306073e3a4bbce15180ce3acf456b8b56b818
tae-sk <68097756+tae-sk@users.noreply.github.com>
2020-12-15 13:52:09 +0900

    feat: System 값을 받아올 Register를 만든다.

---
+++ src/main/java/com/taeseok/apis/vo/UserRegisterVO.java
@@ -0,0 +1,22 @@
+package com.taeseok.apis.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserRegisterVO {
+    private String email;
+    private String name;
+    private String phone;
+
+    @Override
+    public String toString(){
+        return String.format(
+                "UserRegisterVO[email='%s', name='%s', phone='%s']",
+                this.email, this.name, this.phone
+        );
+    }
+}
